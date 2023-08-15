84ead4c4e184c93199af80e7d54b7c9a33829248
macarious <63441014+macarious@users.noreply.github.com>
2023-08-15 02:08:10 -0700

    Fix gap on spreadsheet

--- src/Components/SpreadSheet.tsx
+++ src/Components/SpreadSheet.tsx
@@ -324,7 +324,7 @@ function SpreadSheet({ documentName }: SpreadSheetProps) {
   return (
     <div className="spreadsheet-container">
       <div className="sheet-card d-flex flex-column">
-        <div className="d-flex flex-row justify-content-between mt-5 gap-3">
+        <div className="d-flex flex-row justify-content-between mt-2 gap-3">
           <p className="mt-1">
             <b>Document Name: </b>
             <Badge bg="warning" style={{ color: "black", fontSize: "1.1rem" }}>
